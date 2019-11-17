d3388162f7831caddb3328c782c19308ca1425c1
Daniel Gonçalves <daniel.gonc@lves.fr>
2019-11-17 21:53:20 +0100

    Create a python module

---
+++ dockerDNS/__init__.py
@@ -0,0 +1,5 @@
+from .dockerDNS import DNS_PORT, LISTEN_ADDRESS, DockerResolver, EventsListener, run
+
+__all__ = ["DNS_PORT", "LISTEN_ADDRESS", "DockerResolver", "EventsListener", "run"]
+
+__version__ = 0.1
--- dockerDNS/dockerDNS.py
+++ dockerDNS/dockerDNS.py
@@ -7,7 +7,6 @@ Resolve docker container's name into IPv4 address
 
 import os
 import docker
-import argparse
 from threading import Thread
 from twisted.internet import reactor, defer
 from twisted.names import client, dns, server
@@ -74,29 +73,7 @@ class EventsListener(Thread):
         self.resolver.removeContainer(containerName)
 
 
-def getForwarders(forwarders=None, listenAddress=LISTEN_ADDRESS):
-    """
-    Reads forwarders from arguments or from resolv.conf and create a list of
-    tuples containing the forwarders' IP and the port.
-    """
-    if forwarders is None:
-        forwarders = []
-        with open("/etc/resolv.conf", "r") as resolvconf:
-            for line in resolvconf:
-                if line.startswith("nameserver"):
-                    if line[11:-1] == listenAddress:
-                        continue
-                    else:
-                        forwarders.append((line[11:-1], DNS_PORT))
-            if len(forwarders) == 0:
-                forwarders = None
-    else:
-        forwarders = forwarders.split(",")
-        forwarders = [(address, DNS_PORT) for address in forwarders]
-    return forwarders
-
-
-def dockerDns(port=DNS_PORT, listenAddress=LISTEN_ADDRESS, forwarders=None):
+def run(port=DNS_PORT, listenAddress=LISTEN_ADDRESS, forwarders=None):
     """Configure and execute the DNS server."""
     dockerClient = docker.from_env()
     resolver = DockerResolver(dockerClient=dockerClient,
@@ -112,22 +89,3 @@ def dockerDns(port=DNS_PORT, listenAddress=LISTEN_ADDRESS, forwarders=None):
     # For an unknown reason sys.exit() does not work
     # so we use this hack.
     os._exit(0)
-
-
-if __name__ == "__main__":
-    description = "Resolve docker container's name into IPv4 address"
-    parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("--port",
-                        type=int,
-                        default=DNS_PORT)
-    parser.add_argument("--listen-address",
-                        dest="listenAddress",
-                        default=LISTEN_ADDRESS)
-    parser.add_argument("--forwarders",
-                        default=None)
-    options = parser.parse_args()
-    forwarders = getForwarders(forwarders=options.forwarders,
-                               listenAddress=options.listenAddress)
-    dockerDns(port=options.port,
-              listenAddress=options.listenAddress,
-              forwarders=forwarders)
